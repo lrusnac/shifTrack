2f34dffef177771640d0395326b89831c467f4a2
Leonid <leonidrusnac4@gmail.com>
2015-11-03 17:32:52 +0100

    forgot to link the last view controller

--- shifTrack/Model/Shift.swift
+++ shifTrack/Model/Shift.swift
@@ -4,4 +4,10 @@ import CoreData
 
 class Shift: NSManagedObject {
 
+    func getDuration() -> NSTimeInterval? {
+        if let endTime = finishTime {
+            return endTime.timeIntervalSinceDate(startTime!)
+        }
+        return nil
+    }
 }
