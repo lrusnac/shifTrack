4ae270dcad5730cf5eadf34ab689898d3b644f5e
Leonid <leonidrusnac4@gmail.com>
2015-11-03 15:52:39 +0100

    first core data tests

---
+++ shifTrack/Location.swift
@@ -0,0 +1,17 @@
+//
+//  Location.swift
+//  shifTrack
+//
+//  Created by Leonid Rusnac on 03/11/15.
+//  Copyright © 2015 Leonid Rusnac. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Location: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
