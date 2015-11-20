cc51c030bc75a0dd29a6c3d19cb5e1634e382d71
Leonid <leonidrusnac4@gmail.com>
2015-11-20 08:43:32 +0100

    app icon added for the iphone version

--- shifTrack/ShiftViewController/ShiftDetailViewController.swift
+++ shifTrack/ShiftViewController/ShiftDetailViewController.swift
@@ -2,19 +2,53 @@ import UIKit
 
 class ShiftDetailViewController: UITableViewController {
 
-    // var canBeSaved show or not the save button
+    var tempRowIndex: Int? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        print("select \(indexPath.item)")
-        //let tv = UITableView()
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        let currentSection = indexPath.section
+        let nextRow = indexPath.row + 1
         
-        
-        
-        //tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Bottom)
+        if let tempRow = tempRowIndex { //if a row was previously selected then first close it
+            tempRowIndex = nil
+            tableView.deleteRowsAtIndexPaths([NSIndexPath(forRow: tempRow, inSection: currentSection)], withRowAnimation: UITableViewRowAnimation.Bottom)
+        } else {
+            tempRowIndex = nextRow
+            tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: nextRow, inSection: currentSection)], withRowAnimation: UITableViewRowAnimation.Bottom)
+        }
     }
     
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tempRowIndex != nil {
+            return 4
+        } else {
+            return 3
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var identifier = "locationSelect"
+        
+        
+        if let tempRow = tempRowIndex {
+            if tempRow == indexPath.row {
+                identifier = "timeStartSelect"
+            }
+        }
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(identifier)!
+        
+        return cell
+    }
 }
